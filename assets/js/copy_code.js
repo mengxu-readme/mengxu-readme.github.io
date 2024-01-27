3d759f6a3462981bca2a1b97960edec06b6179ae
Meng Xu <mengxu.gt@gmail.com>
2024-01-27 11:47:04 -0500

    Rebase from upstream Commit: f6bb6b5dcb4411c539905522f928e2551e523034

--- assets/js/copy_code.js
+++ assets/js/copy_code.js
@@ -3,8 +3,11 @@ var codeBlocks = document.querySelectorAll("pre");
 codeBlocks.forEach(function (codeBlock) {
   if (
     (codeBlock.querySelector("pre:not(.lineno)") || codeBlock.querySelector("code")) &&
-    codeBlock.querySelector("code:not(.language-chart)") &&
-    codeBlock.querySelector("code:not(.language-mermaid)")
+    codeBlock.querySelector("code:not(.language-chartjs)") &&
+    codeBlock.querySelector("code:not(.language-echarts)") &&
+    codeBlock.querySelector("code:not(.language-geojson)") &&
+    codeBlock.querySelector("code:not(.language-mermaid)") &&
+    codeBlock.querySelector("code:not(.language-vega_lite)")
   ) {
     // create copy button
     var copyButton = document.createElement("button");
