3f5e879792028bccd7455777f2cbd3227093282c
mengxu-readme <57160057+mengxu-readme@users.noreply.github.com>
2025-08-26 00:19:46 +0000

    Deploying to gh-pages from @ mengxu-readme/mengxu-readme.github.io@ab0756feefa5d8081569801b884a9fd855c3750a 🚀

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,42 @@
+function determineGiscusTheme() {
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
+
+    if (theme === "dark") return "";
+    if (theme === "light") return "";
+
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "" : "";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "mengxu-readme/mengxu-readme.github.io",
+    "data-repo-id": "",
+    "data-category": "",
+    "data-category-id": "",
+    "data-mapping": "",
+    "data-strict": "",
+    "data-reactions-enabled": "",
+    "data-emit-metadata": "",
+    "data-input-position": "",
+    "data-theme": giscusTheme,
+    "data-lang": "",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
